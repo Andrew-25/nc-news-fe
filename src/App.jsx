abb8c5309fddfaa5fd85974146b245ffa60e4dda
Andrew-25 <andrewoozs@hotmail.com>
2023-12-11 15:13:29 +0000

    main updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Articles from './components/Articles'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Routes>
+        <Route path='/' element={<Articles />}></Route>
 
-      <Articles />
+      </Routes>
     </>
   )
 }
